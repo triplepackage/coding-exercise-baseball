9467168646fb0109d208bf7862d4f21dbd171916
John <admin@tests-MBP-3.fios-router.home>
2019-09-21 17:33:25 -0400

    Added more unit tests.

--- src/tests/PlayerGamePerformanceTests.java
+++ src/tests/PlayerGamePerformanceTests.java
@@ -16,4 +16,40 @@ public class PlayerGamePerformanceTests {
         float average = performance.getGameBattingAverage();
         Assert.assertTrue(average == 1.0f);
     }
+
+    @Test
+    public void testBattingAverage25() {
+        PlayerGamePerformance performance = new PlayerGamePerformance(new Player("f081095a-7d7b-11e9-8f9e-2a86e4085a59", "John", "Doe"),
+                4, 1, 0, 0, 0);
+
+        float average = performance.getGameBattingAverage();
+        Assert.assertTrue(average == 0.25f);
+    }
+
+    @Test
+    public void testBattingAverage50() {
+        PlayerGamePerformance performance = new PlayerGamePerformance(new Player("f081095a-7d7b-11e9-8f9e-2a86e4085a59", "John", "Doe"),
+                4, 1, 0, 0, 1);
+
+        float average = performance.getGameBattingAverage();
+        Assert.assertTrue(average == 0.5f);
+    }
+
+    @Test
+    public void testSluggingPercentage() {
+        PlayerGamePerformance performance = new PlayerGamePerformance(new Player("f081095a-7d7b-11e9-8f9e-2a86e4085a59", "John", "Doe"),
+                4, 1, 0, 0, 0);
+
+        float average = performance.getGameBattingAverage();
+        Assert.assertTrue(average == 0.25f);
+    }
+
+    @Test
+    public void testSluggingPercentage25() {
+        PlayerGamePerformance performance = new PlayerGamePerformance(new Player("f081095a-7d7b-11e9-8f9e-2a86e4085a59", "John", "Doe"),
+                4, 1, 1, 1, 1);
+
+        float average = performance.getGameSluggingPercentage();
+        Assert.assertTrue(average == 2.5f);
+    }
 }
